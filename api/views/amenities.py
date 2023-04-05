4e5b1bb468c8139973417bcf643e52f45af77d56
acidicForest0413 <112514043+acidicForest0413@users.noreply.github.com>
2023-04-04 21:35:00 -0500

    Update amenities.py

--- api/views/amenities.py
+++ api/views/amenities.py
@@ -1,57 +1,54 @@
 #!/usr/bin/python3
-"""
-    Flask route that returns json respone
-"""
-from api.v1.views import app_views
-from flask import abort, jsonify, request
-from models import storage, CNC
-from flasgger.utils import swag_from
+"""Flask route module for amenities"""
+from api.v1.views import app_views, validate_model, get_json
+from flask import jsonify
+from models import storage, class_dictionary
 
 
-@app_views.route('/amenities/', methods=['GET', 'POST'])
-@swag_from('swagger_yaml/amenities_no_id.yml', methods=['GET', 'POST'])
-def amenities_no_id(amenity_id=None):
-    """
-        amenities route that handles http requests no ID given
-    """
-    if request.method == 'GET':
-        all_amenities = storage.all('Amenity')
-        all_amenities = [obj.to_json() for obj in all_amenities.values()]
-        return jsonify(all_amenities)
-
-    if request.method == 'POST':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        if req_json.get('name') is None:
-            abort(400, 'Missing name')
-        Amenity = CNC.get('Amenity')
-        new_object = Amenity(**req_json)
-        new_object.save()
-        return jsonify(new_object.to_json()), 201
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def amenities_no_id_get(amenity_id=None):
+    """amenities route - no id GET scenario"""
+    all_amenities = storage.all('Amenity')
+    all_amenities = [obj.to_dict() for obj in all_amenities.values()]
+    return jsonify(all_amenities)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET', 'DELETE', 'PUT'])
-@swag_from('swagger_yaml/amenities_id.yml', methods=['GET', 'DELETE', 'PUT'])
-def amenities_with_id(amenity_id=None):
-    """
-        amenities route that handles http requests with ID given
-    """
-    amenity_obj = storage.get('Amenity', amenity_id)
-    if amenity_obj is None:
-        abort(404, 'Not found')
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def amenities_no_id_post(amenity_id=None):
+    """amenities route - no id POST scenario"""
+    req_json = get_json(['name'])
+    Amenity = class_dictionary.get('Amenity')
+    new_object = Amenity(**req_json)
+    new_object.save()
+    return jsonify(new_object.to_dict()), 201
 
-    if request.method == 'GET':
-        return jsonify(amenity_obj.to_json())
 
-    if request.method == 'DELETE':
-        amenity_obj.delete()
-        del amenity_obj
-        return jsonify({}), 200
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
+def amenities_with_id_get(amenity_id=None):
+    """amenities route - id given GET scenario"""
+    amenity_obj = validate_model('Amenity', amenity_id)
+    return jsonify(amenity_obj.to_dict())
 
-    if request.method == 'PUT':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        amenity_obj.bm_update(req_json)
-        return jsonify(amenity_obj.to_json()), 200
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def amenities_with_id_del(amenity_id=None):
+    """amenities route - id given DELETE scenario"""
+    amenity_obj = validate_model('Amenity', amenity_id)
+    amenity_obj.delete()
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
+def amenities_with_id_put(amenity_id=None):
+    """amenities route - id given PUT scenario"""
+    amenity_obj = validate_model('Amenity', amenity_id)
+    req_json = get_json()
+    for key, value in req_json.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(amenity_obj, key, value)
+    amenity_obj.save()
+    return jsonify(amenity_obj.to_dict()), 200
